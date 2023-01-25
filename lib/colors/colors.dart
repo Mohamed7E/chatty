c5c2a2b865d7c4ae5e17c0d7f1f2a5a395ffb598
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2023-01-25 16:21:33 +0200

    colors

---
+++ lib/colors/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const bgcolor=Color.fromARGB(255, 52, 121, 177);
+const textcolor=Color.fromARGB(255, 10, 12, 14);
+const chatcolor=Color.fromARGB(255, 55, 64, 64);
